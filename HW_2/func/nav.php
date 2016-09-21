246a291de6322b557bee1f8841bfd25e60d207c8
ihorinho <ihorinho@gmail.com>
2016-09-21 14:31:33 +0300

    Delete unnecessary rows

--- HW_2/func/nav.php
+++ HW_2/func/nav.php
@@ -7,31 +7,5 @@
 				echo "><a href='index.php?id={$i}''>Завдання {$i}</a></li>";
 			}
 			?>
-			<!-- <li <?=$id==1 ? 'class="active"' : ''?>><a href="index.php?id=1">Завдання 1</a></li>
-			<li <?=$id==2 ? 'class="active"' : ''?>><a href="index.php?id=2">Завдання 2</a></li>
-			<li <?=$id==3 ? 'class="active"' : ''?>><a href="index.php?id=3">Завдання 3</a></li>
-			<li <?=$id==4 ? 'class="active"' : ''?>><a href="index.php?id=4">Завдання 4</a></li>
-			<li <?=$id==5 ? 'class="active"' : ''?>><a href="index.php?id=5">Завдання 5</a></li>
-			<li <?=$id==6 ? 'class="active"' : ''?>><a href="index.php?id=6">Завдання 6</a></li>
-			<li <?=$id==7 ? 'class="active"' : ''?>><a href="index.php?id=7">Завдання 7</a></li>
-			<li <?=$id==8 ? 'class="active"' : ''?>><a href="index.php?id=8">Завдання 8</a></li>
-			<li <?=$id==9 ? 'class="active"' : ''?>><a href="index.php?id=9">Завдання 9</a></li>
-			<li <?=$id==10 ? 'class="active"' : ''?>><a href="index.php?id=10">Завдання 10</a></li>
-			<li <?=$id==11 ? 'class="active"' : ''?>><a href="index.php?id=11">Завдання 11</a></li>
-			<li <?=$id==12 ? 'class="active"' : ''?>><a href="index.php?id=12">Завдання 12</a></li>
-			<li <?=$id==13 ? 'class="active"' : ''?>><a href="index.php?id=13">Завдання 13</a></li>
-			<li <?=$id==14 ? 'class="active"' : ''?>><a href="index.php?id=14">Завдання 14</a></li>
-			<li <?=$id==15 ? 'class="active"' : ''?>><a href="index.php?id=15">Завдання 15</a></li>
-			<li <?=$id==16 ? 'class="active"' : ''?>><a href="index.php?id=16">Завдання 16</a></li>
-			<li <?=$id==17 ? 'class="active"' : ''?>><a href="index.php?id=17">Завдання 17</a></li>
-			<li <?=$id==18 ? 'class="active"' : ''?>><a href="index.php?id=18">Завдання 18</a></li>
-			<li <?=$id==19 ? 'class="active"' : ''?>><a href="index.php?id=19">Завдання 19</a></li>
-			<li <?=$id==20 ? 'class="active"' : ''?>><a href="index.php?id=20">Завдання 20</a></li>
-			<li <?=$id==21 ? 'class="active"' : ''?>><a href="index.php?id=21">Завдання 21</a></li>
-			<li <?=$id==22 ? 'class="active"' : ''?>><a href="index.php?id=22">Завдання 22</a></li>
-			<li <?=$id==23? 'class="active"' : ''?>><a href="index.php?id=23">Завдання 23</a></li>
-			<li <?=$id==24? 'class="active"' : ''?>><a href="index.php?id=24">Завдання 24</a></li>
-			<li <?=$id==25? 'class="active"' : ''?>><a href="index.php?id=25">Завдання 25</a></li>
-			<li <?=$id==26? 'class="active"' : ''?>><a href="index.php?id=26">Завдання 26</a></li> -->
 		</ul>
 	</div>
