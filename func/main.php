e58e565dbbbbbbd3fae8be51c49386bbe39411ce
ihorinho <ihorinho@gmail.com>
2016-10-09 15:02:22 +0300

    Added 6th task about functions

--- func/main.php
+++ func/main.php
@@ -3,6 +3,8 @@
 	<li><a href="HW_1_resume_with_Bootstrap/index.php">Домашка №1 Резюме (з Bootstrap)</a></li>
 	<li><a href="HW_2_PHP_fundamentals/index.php">Домашка №2 Задачі по основах PHP</a></li>
 	<li><a href="index.php?id=4">Домашка №3 Масиви і цикли</a></li>
-	<li><a href="CW_loops_and_books/">Аудит. робота 20_09 Книжки блоками і табл. множення</a></li>
 	<li><a href="HW_4_Functions/index.php">Домашка №4 Функції</a></li>
+	<li><a href="CW_loops_and_books/">Аудит. робота 20_09 Книжки блоками і табл. множення</a></li>
+	<li><a href="CW_2_Functions/">Аудит. робота 04_10 Функції. Комент. форма</a></li>
+
 </nav>
